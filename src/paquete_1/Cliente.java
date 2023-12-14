905689b64930ff0cbdbcd1a52012a86373e8d4ec
SergioSevaRayos <sergiosevarayos@gmail.com>
2023-12-14 20:47:16 +0100

    v0.0 Creando la clase Cliente

--- src/paquete_1/Cliente.java
+++ src/paquete_1/Cliente.java
@@ -1,5 +1,13 @@
 package paquete_1;
 
-public class Cliente {
+import java.util.*;
 
+public class Cliente {
+	String nombre;
+	static Date FechaDeAlta = new Date();
+	
+	
+	public static void main(String[] args) {
+		System.out.println(FechaDeAlta);
+	}
 }
