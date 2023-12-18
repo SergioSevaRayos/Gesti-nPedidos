12b7eb0744920f1a6ef7f56da5832a2950ddcfc8
SergioSevaRayos <sergiosevarayos@gmail.com>
2023-12-18 20:45:19 +0100

    v0.0 Creando el método para introducir el teléfono

--- src/paquete_1/Cliente.java
+++ src/paquete_1/Cliente.java
@@ -3,11 +3,69 @@ package paquete_1;
 import java.util.*;
 
 public class Cliente {
-	String nombre;
-	static Date FechaDeAlta = new Date();
+	 // Atributos
+	static String nombre;
+	static String apellidos;
+	static Date FechaDeAlta;
+	static String telefono;
+	static String direccion;
+	static String historial;
+	// Variables
+	static Scanner sc = new Scanner(System.in);
+	static String nombreMinuscula;
+	static String apellidosMayuscula;
 	
+	// Constructor
+	public Cliente(String nombre, String apellidos, Date FechaDeAlta, String telefono, String direccion, String historial) {
+		Cliente.nombre = nombre;
+		Cliente.apellidos = apellidos;
+		Cliente.FechaDeAlta = FechaDeAlta;
+		Cliente.telefono = telefono;
+		Cliente.direccion = direccion;
+		Cliente.historial = historial;
+	}
+	
+	public static void inNombreYapellidos() {
+		System.out.print("Introduce el nombre --> ");
+		nombre = sc.nextLine();
+		nombreMinuscula = nombre.toLowerCase();
+		System.out.print("Introduce los apellidos --> ");
+		apellidos = sc.nextLine();
+		apellidosMayuscula = apellidos.toUpperCase();
+	}
+	
+	public static void fechaAlta() {
+		FechaDeAlta = new Date();
+	}
+
+	public static void inTelefono() {
+
+		System.out.print("Introduce el teléfono --> ");
+		telefono = sc.nextLine();
+		if (telefono.length() != 9) {
+			System.out.println("El teléfono es incorrecto, vuelva a intentarlo");
+			inTelefono();
+		}else {
+			System.out.println("El teléfono introducido es correcto");
+		}
+
+
+	}
+	
+	public static void datos() {
+		System.out.println(
+				  "Nombre        -->\t" + nombreMinuscula + "\n"
+				+ "Apellidos     -->\t" + apellidosMayuscula + "\n"
+				+ "Fecha de alta -->\t" + FechaDeAlta);
+		
+	}
 	
 	public static void main(String[] args) {
-		System.out.println(FechaDeAlta);
+		inTelefono();
+//		inNombreYapellidos();
+//		fechaAlta();
+//		datos();
+//		System.out.println(FechaDeAlta);
+		
 	}
 }
